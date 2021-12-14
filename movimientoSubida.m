0e2caf2d01dc71845add9b399831be01be7dc395
luucia15 <63645788+luucia15@users.noreply.github.com>
2021-12-14 11:57:32 +0100

    corrección de errores

--- movimientoSubida.m
+++ movimientoSubida.m
@@ -41,97 +41,88 @@ piernaSuperior2=[-10,-5,120;15,-5,120;15,20,120;-10,20,120;-10,-5,140;15,-5,140;
 torso1 = [-10,-5,140;15,-5,140;15,55,140;-10,55,140;-10,-10,250;15,-10,250;15,60,250;-10,60,250];
 torso= [-10,-5,140;2,-5,140;2,55,140;-10,55,140;-10,-5,150;2,-5,150;2,55,150;-10,55,150];
 
-        Rtorso = rotationMatrix(0,0,0);
-        Rcadera = rotationMatrix(0,0,0);
-        Rrodilla = rotationMatrix(0,0,0);
-        Rtobillo = rotationMatrix(0,0,0);
-
+Rtorso = rotationMatrix(0,0,0);
+Rcadera = rotationMatrix(0,0,0);
+Rrodilla = rotationMatrix(0,0,0);
+Rtobillo = rotationMatrix(0,0,0);
 
 % Angulos de cadera en subida
 y_cs = [8,9,11,12,10,9,6,5,4,4,3,0,-15,-18,-15,-10,3,6,10,10,8];
-% Angulos de cadera en bajada
-y_cb = [46,40,35,30,25,15,10,5,2,-2,-4,-6,-5,0,10,23,34,45,47,49,46];
 % Angulos de rodilla en subida
 y_rs = [6,10,20,26,28,27,26,27,35,45,55,55,75,87,93,87,65,50,30,10,6];
-% Angulos de rodilla en bajada
-y_rb = [60,58,56,53,45,35,26,22,12,8,7,7,10,23,45,65,85,94,90,75,60];
 % Angulos de tobillo en subida
 y_ts = [-20,-10,5,7,10,12,13,15,19,22,24,20,10,2,1,-2,-10,-15,-20,-26,-20];
-% Angulos de tobillo en bajada
-y_tb = [11,10,13,14,11,8,7,6,7,9,10,14,19,25,29,31,29,25,20,15,11];
 
 fm =10;
 % Movimiento progresivo de subida
-for ii = 1:size(y_cs,2)
-    Rtorso = rotationMatrix(0,0,0);
+    for ii = 1:size(y_cs,2)
+        Rtorso = rotationMatrix(0,0,0);
+        cla
+        tic
+        torsoRotado = applyRotationNx3(torso,sum(torso)/8,Rtorso);
+        torsoRotado1 = applyRotationNx3(torso1,sum(torso)/8,Rtorso);
     
-cla
-tic
-    torsoRotado = applyRotationNx3(torso,sum(torso)/8,Rtorso);
-    torsoRotado1 = applyRotationNx3(torso1,sum(torso)/8,Rtorso);
-
-    patch('faces',f,'vertices',torsoRotado,'FaceVertexCData',color,'FaceColor','flat');
-    patch('faces',f,'vertices',torsoRotado1,'FaceVertexCData',color,'FaceColor','flat');
-
-    Rcadera = rotationMatrix(0,y_cs(ii),0);
-
-    piernaSuperiorRotada1 = applyRotationNx3(applyRotationNx3(piernaSuperior1,...
-        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-    piernaSuperiorRotada2 = applyRotationNx3(applyRotationNx3(piernaSuperior2,...
-        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-
-    patch('faces',f,'vertices',piernaSuperiorRotada1,'FaceVertexCData',color,'FaceColor','flat');
-    patch('faces',f,'vertices',piernaSuperiorRotada2,'FaceVertexCData',color,'FaceColor','flat');
-
-%         for jj = 1:size(y_rs,2)
-            Rrodilla = rotationMatrix(0,y_rs(ii),0);
-            
-            piernaInferiorRotada1 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna1,...
-                sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-            piernaInferiorRotada2 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna2,...
-                sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-            piernaInferiorRotada3 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna3,...
-                sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-
-            patch('faces',f,'vertices',piernaInferiorRotada1,'FaceVertexCData',color,'FaceColor','flat');
-            patch('faces',f,'vertices',piernaInferiorRotada2,'FaceVertexCData',color,'FaceColor','flat');
-            patch('faces',f,'vertices',piernaInferiorRotada3,'FaceVertexCData',color,'FaceColor','flat');
-               
-%                 for kk = 1:size(y_ts,2)
-                    Rtobillo = rotationMatrix(0,y_ts(ii),0);
-
-                    pieTalonRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(pieDTalon,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-                    piePuntaRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(pieDPunta,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-                    dedoPulgarRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoGordo,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-                    dedoIndiceRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoIndice,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-                    dedoCorazonRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoCorazon,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-                    dedoAnularRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoAnular,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-                    dedoMeniqueRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoMenique,...
-                        sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
-                        (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
-
-                    patch('faces',f,'vertices',pieTalonRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    patch('faces',f,'vertices',piePuntaRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    patch('faces',f,'vertices',dedoPulgarRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    patch('faces',f,'vertices',dedoIndiceRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    patch('faces',f,'vertices',dedoCorazonRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    patch('faces',f,'vertices',dedoAnularRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    patch('faces',f,'vertices',dedoMeniqueRotado,'FaceVertexCData',color,'FaceColor','flat');
-                    view(3);
-                    axis image;
-                    tiempo = toc;
-                pause(1/fm-tiempo);
-                    drawnow;
+        patch('faces',f,'vertices',torsoRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',torsoRotado1,'FaceVertexCData',color,'FaceColor','flat');
+    
+        Rcadera = rotationMatrix(0,y_cs(ii),0);
+    
+        piernaSuperiorRotada1 = applyRotationNx3(applyRotationNx3(piernaSuperior1,...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        piernaSuperiorRotada2 = applyRotationNx3(applyRotationNx3(piernaSuperior2,...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+    
+        patch('faces',f,'vertices',piernaSuperiorRotada1,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',piernaSuperiorRotada2,'FaceVertexCData',color,'FaceColor','flat');
+    
+        Rrodilla = rotationMatrix(0,y_rs(ii),0);
+    
+        piernaInferiorRotada1 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna1,...
+            sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        piernaInferiorRotada2 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna2,...
+            sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        piernaInferiorRotada3 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna3,...
+            sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+    
+        patch('faces',f,'vertices',piernaInferiorRotada1,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',piernaInferiorRotada2,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',piernaInferiorRotada3,'FaceVertexCData',color,'FaceColor','flat');
+    
+        Rtobillo = rotationMatrix(0,y_ts(ii),0);
+    
+        pieTalonRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(pieDTalon,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        piePuntaRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(pieDPunta,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        dedoPulgarRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoGordo,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        dedoIndiceRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoIndice,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        dedoCorazonRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoCorazon,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        dedoAnularRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoAnular,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+        dedoMeniqueRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(dedoMenique,...
+            sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
+            (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
+    
+        patch('faces',f,'vertices',pieTalonRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',piePuntaRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',dedoPulgarRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',dedoIndiceRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',dedoCorazonRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',dedoAnularRotado,'FaceVertexCData',color,'FaceColor','flat');
+        patch('faces',f,'vertices',dedoMeniqueRotado,'FaceVertexCData',color,'FaceColor','flat');
+        view(3);
+        axis image;
+        tiempo = toc;
+        pause(1/fm-tiempo);
+        drawnow;
+    end
 end
