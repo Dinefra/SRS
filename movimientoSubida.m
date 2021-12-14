17c5ab3d7ea3bbbfb2b28f274a9648a4e4eedb27
luucia15 <63645788+luucia15@users.noreply.github.com>
2021-12-14 11:21:10 +0100

    Revert "corrección de errores"
    This reverts commit 40d67b8ec1fdff0cc6aedbe1eda0bddc3ca170d5.

--- movimientoSubida.m
+++ movimientoSubida.m
@@ -46,15 +46,22 @@ torso= [-10,-5,140;2,-5,140;2,55,140;-10,55,140;-10,-5,150;2,-5,150;2,55,150;-10
         Rrodilla = rotationMatrix(0,0,0);
         Rtobillo = rotationMatrix(0,0,0);
 
+
 % Angulos de cadera en subida
 y_cs = [8,9,11,12,10,9,6,5,4,4,3,0,-15,-18,-15,-10,3,6,10,10,8];
+% Angulos de cadera en bajada
+y_cb = [46,40,35,30,25,15,10,5,2,-2,-4,-6,-5,0,10,23,34,45,47,49,46];
 % Angulos de rodilla en subida
 y_rs = [6,10,20,26,28,27,26,27,35,45,55,55,75,87,93,87,65,50,30,10,6];
+% Angulos de rodilla en bajada
+y_rb = [60,58,56,53,45,35,26,22,12,8,7,7,10,23,45,65,85,94,90,75,60];
 % Angulos de tobillo en subida
 y_ts = [-20,-10,5,7,10,12,13,15,19,22,24,20,10,2,1,-2,-10,-15,-20,-26,-20];
+% Angulos de tobillo en bajada
+y_tb = [11,10,13,14,11,8,7,6,7,9,10,14,19,25,29,31,29,25,20,15,11];
 
 % Movimiento progresivo de subida
-for i = 1:size(y_cs,2)
+for ii = 1:size(y_cs,2)
     Rtorso = rotationMatrix(0,0,90);
 
     torsoRotado = applyRotationNx3(torso,sum(torso)/8,Rtorso);
@@ -64,7 +71,7 @@ for i = 1:size(y_cs,2)
     patch('faces',f,'vertices',torsoRotado,'FaceVertexCData',color,'FaceColor','flat');
     patch('faces',f,'vertices',torsoRotado1,'FaceVertexCData',color,'FaceColor','flat');
 
-    Rcadera = rotationMatrix(0,y_cs(i),90);
+    Rcadera = rotationMatrix(0,y_cs(ii),90);
 
     piernaSuperiorRotada1 = applyRotationNx3(applyRotationNx3(piernaSuperior1,...
         (torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
@@ -74,8 +81,8 @@ for i = 1:size(y_cs,2)
     patch('faces',f,'vertices',piernaSuperiorRotada1,'FaceVertexCData',color,'FaceColor','flat');
     patch('faces',f,'vertices',piernaSuperiorRotada2,'FaceVertexCData',color,'FaceColor','flat');
 
-        for j = 1:size(y_rs,2)
-            Rrodilla = rotationMatrix(0,y_rs(j),90);
+        for jj = 1:size(y_rs,2)
+            Rrodilla = rotationMatrix(0,y_rs(jj),90);
             
             piernaInferiorRotada1 = applyRotationNx3(applyRotationNx3(applyRotationNx3(Pierna1,...
                 sum(piernaSuperior1(1:4,:))/4,Rrodilla),(torso(2,:)+torso(3,:))/2,Rcadera),sum(torso)/8,Rtorso);
@@ -88,8 +95,8 @@ for i = 1:size(y_cs,2)
             patch('faces',f,'vertices',piernaInferiorRotada2,'FaceVertexCData',color,'FaceColor','flat');
             patch('faces',f,'vertices',piernaInferiorRotada3,'FaceVertexCData',color,'FaceColor','flat');
                
-                for k = 1:size(y_ts,2)
-                    Rtobillo = rotationMatrix(0,y_ts(k),90);
+                for kk = 1:size(y_ts,2)
+                    Rtobillo = rotationMatrix(0,y_ts(kk),90);
 
                     pieTalonRotado = applyRotationNx3(applyRotationNx3(applyRotationNx3(applyRotationNx3(pieDTalon,...
                         sum(Pierna1(1:4,:))/4,Rtobillo),sum(piernaSuperior1(1:4,:))/4,Rrodilla), ...
@@ -122,9 +129,7 @@ for i = 1:size(y_cs,2)
                     patch('faces',f,'vertices',dedoMeniqueRotado,'FaceVertexCData',color,'FaceColor','flat');
                     view(3);
                     axis image;
-                    drawnow;
                 end
         end
-end
-
+       cla reset
 end
