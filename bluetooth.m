0e5b5c7bcc8bd618f080f6320fd08ed325e488f8
Lucía Herrador <luciahd00@gmail.com>
2022-05-04 11:24:10 +0200

    conexión bluetooth

---
+++ bluetooth.m
@@ -0,0 +1,15 @@
+bluetoothlist;
+% Para saber cómo se llama el dispositivo que queremos conectar y en qué 
+% canal está. Sólo se hace la primera vez para configurarlo. Para ello 
+% previamente se debe haber vinculado el dispositivo al ordenador con
+% Bluetooth.
+
+device = bluetooth("ESP32Test", 1); 
+% Generamos una variable llamada device que contiene todas las propiedades 
+% de la placa especificada conectada por Bluetooth al canal indicado.
+
+writeline(device,"hello"); 
+% Para escribir en el dispositivo lo especificado en Arduino IDE
+
+readline(device);
+% Para leer lo especificado en Arduino IDE
