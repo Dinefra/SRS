3548a9a45aa8cbb71436018d49ce2dbe197b6693
isagcasamayor <63645082+isagcasamayor@users.noreply.github.com>
2022-06-08 20:13:56 +0200

    código limpio

--- README.md
+++
@@ -1,2 +0,0 @@
-# SRS
- Stair Rehabilitation Studio
--- prueba.m
+++
@@ -1,7 +0,0 @@
-%Documento Prueba para ver que funcione bien Github
-%% Codigo colores
-% Boton avatar- informe #a1d1ff
-% Fondo interfaz #deefff
-%
-
-
